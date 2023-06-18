91bf5ae4cf7b1494ac1b284f493239756f64d50b
Kevin Zhou Mastascusa <kzhoum@gmail.com>
2023-06-18 14:32:04 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -17,39 +17,3 @@ ui <- fluidPage(
     )
   )
 )
-
-# Server
-server <- function(input, output) {
-
-  # Calculate GC content
-  observeEvent(input$calculate_gc, {
-    dna_sequence <- input$dna_sequence
-    gc_content <- calculate_gc_content(dna_sequence)
-    output$gc_content_output <- renderPrint({
-      paste("GC Content:", gc_content)
-    })
-  })
-
-  # Count nucleotides
-  observeEvent(input$count_nucleotides, {
-    dna_sequence <- input$dna_sequence
-    nucleotide_counts <- count_nucleotides(dna_sequence)
-    output$nucleotide_counts_output <- renderPrint({
-      paste("Nucleotide Counts:")
-      print(nucleotide_counts)
-    })
-  })
-
-  # Translate sequence
-  observeEvent(input$translate_sequence, {
-    dna_sequence <- input$dna_sequence
-    translated_sequence <- translate_sequence(dna_sequence)
-    output$translated_sequence_output <- renderPrint({
-      paste("Translated Sequence:")
-      print(translated_sequence)
-    })
-  })
-}
-
-# Run the Shiny app
-shinyApp(ui, server)
